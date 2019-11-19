0e49dc8ade95ed2775b7ea6639a01f81c5eaff9b
satoshiking <samoilovn@mail.ru>
2019-11-19 17:06:36 +0300

    Test tasks

--- teleport/postamat.py
+++ teleport/postamat.py
@@ -1,18 +1,12 @@
-import requests
-import json
 from datetime import datetime
-
-def pretty_print_json(data):
-    for st in data:
-        print(json.dumps(st, indent="   ",ensure_ascii=False, sort_keys=True))
 
 
 class Postamat:
 
-    def __init__(self, 
-                 id, address, is_automated, accept_cash, accept_card, bank_terminal,
-                 status_code=99, lat=None, lng=None, description="",
-                 **kwargs):
+    def __init__(self,
+                 id, address, is_automated, accept_cash, accept_card,
+                 bank_terminal, status_code=99, lat=None, lng=None,
+                 description="", **kwargs):
         self.id = id
         self.address = address
         self.is_automated = is_automated
@@ -32,7 +26,6 @@ class Postamat:
         self._accept_payments = kwargs.get("accept_payments")
         self._working_hours = kwargs.get("working_hours")
 
-
     @property
     def name(self):
         return self._name
@@ -40,7 +33,7 @@ class Postamat:
     @property
     def address_struct(self):
         return self._address_struct
-    
+
     @property
     def status(self):
         return self._status
@@ -54,7 +47,7 @@ class Postamat:
         return self._working_hours
 
     def is_working(self):
-        day = datetime.today().weekday()        
+        day = datetime.today().weekday()
         time = datetime.today().strftime("%H:%M")
         if len(self.working_hours) > day:
             time_open = self.working_hours[day].get("time_open")
@@ -66,16 +59,3 @@ class Postamat:
                 return "No"
         else:
             return "no information"
-
-"""
-if __name__ == '__main__':
-    r = requests.get("https://api.tport.online/v2/public-stations")
-    data = json.loads(r.text)
-    
-    id_list = []
-    for d in data:
-        id_list.append(d["id"])
-
-    print(id_list)
-    #pretty_print_json(data[:1])
-"""
--- teleport/status.py
+++ teleport/status.py
@@ -6,9 +6,9 @@ from postamat import Postamat
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2: 
+    if len(sys.argv) != 2:
         print("Exactly one argument is needed ...")
-        print("Usage: python status.py \"id_list\"."+
+        print("Usage: python status.py \"id_list\"." +
               "Example: python status.py \"10, 20, 30, 55\"")
         sys.exit()
     else:
@@ -17,19 +17,20 @@ if __name__ == '__main__':
         print("Postamat status:")
 
         for id in id_list:
-            r = requests.get("https://api.tport.online/v2/public-stations/" + str(id))
+            url = "https://api.tport.online/v2/public-stations/" + str(id)
+            r = requests.get(url)
             d = json.loads(r.text)
-            
+
             if not d.get("detail"):
                 p = Postamat(
                     d["id"], d["address"], d["is_automated"], d["accept_cash"],
-                    d["accept_card"], d.get("bank_terminal"), d["status_code"], 
+                    d["accept_card"], d.get("bank_terminal"), d["status_code"],
                     d["lat"], d["lng"], d["description"], type=d.get("type"),
                     address_struct=d["address_struct"], status=d["status"],
-                    accept_payments=d["accept_payments"], working_hours=d["working_hours"])
+                    accept_payments=d["accept_payments"],
+                    working_hours=d["working_hours"])
 
-                print(" ID={:<5} STATUS_CODE={:< 5} WORKING_NOW={}".format(p.id, p.status_code, p.is_working()))
+                print(" ID={:<5} STATUS_CODE={:< 5} WORKING_NOW={}".
+                      format(p.id, p.status_code, p.is_working()))
             else:
                 print(" ID={:<5} {: <18}".format(id, d.get("detail")))
-
-
