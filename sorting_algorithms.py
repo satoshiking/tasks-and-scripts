18f9bc96ca8786d2401e12afb9d65b2c8db44b83
satoshiking <samoilovn@mail.ru>
2019-11-26 17:30:09 +0300

    playing with sorting algorithms + unittest

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,5 +1,5 @@
-a = [10, 20, 35, 44, 3, 8]
-b = []
+import unittest
+
 
 # Selection sort algorithm
 def get_smallest(list_to_sort):
@@ -11,13 +11,12 @@ def get_smallest(list_to_sort):
             smallest_index = i
     return smallest_index
 
+
 def sort_by_selection(list_to_sort):
     list_sorted = []
     for i in range(len(list_to_sort)):
         list_sorted.append(list_to_sort.pop(get_smallest(list_to_sort)))
     return list_sorted
-
-#print(sort_by_selection(b))
 
 
 # Quick sort algorithm
@@ -31,4 +30,31 @@ def qsort(list_to_sort):
         return qsort(lower) + [opornii] + qsort(higher)
 
 
-print(qsort(a))
+class TestSortingAlgorithms(unittest.TestCase):
+    def test_sort_by_selection(self):
+        self.assertEqual(sort_by_selection([10, 20, 35, 44, 3, 8]), [3, 8, 10, 20, 35, 44])
+
+    def test_sort_by_selection_sorted(self):
+        self.assertEqual(sort_by_selection([1, 2, 3, 4]), [1, 2, 3, 4])
+
+    def test_sort_by_selection_sorted_decending(self):
+        self.assertEqual(sort_by_selection([96, 37, 14, 8, 3, -5]), [-5, 3, 8, 14, 37, 96])
+
+    def test_sort_by_selection_empty(self):
+        self.assertEqual(sort_by_selection([]), [])
+
+    def test_qsort(self):
+        self.assertEqual(qsort([10, 20, 35, 44, 3, 8]), [3, 8, 10, 20, 35, 44])
+
+    def test_qsort_sorted(self):
+        self.assertEqual(qsort([1, 2, 3, 4]), [1, 2, 3, 4])
+
+    def test_qsort_decending(self):
+        self.assertEqual(qsort([96, 37, 14, 8, 3, -5]), [-5, 3, 8, 14, 37, 96])
+
+    def test_qsort_empty(self):
+        self.assertEqual(qsort([]), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
